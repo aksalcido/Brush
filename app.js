67addaf2ed801bddf047e2d554a4ecb9e3256822
aksalcido <aksalcid@uci.edu>
2020-12-23 05:05:05 -0800

    Login/Register & Profile Page

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
+// Imported Libraries
 var express = require("express"),
     app = express(),
-    mongoose = require("mongoose");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport");
+    LocalStrategy = require("passport-local");
 
+// Imported Models
+var User = require("./models/user.js");
+
+
+// Global Variables
+var BASE_PORT = 3000;
+
+
+
+// Setup (if not created already) and Connect the Mongoose Database 
 mongoose.connect("mongodb://localhost:27017/brush_database", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then( () => console.log("Connected to DB!"))
+.then( () => console.log("Connected to DB!") )
 .catch(error => console.log(error.message));
 
-// Global Variables
-var BASE_PORT = 3000;
+
+// Set up BodyParser to handle HTTP POST reqs
+app.use(bodyParser.urlencoded({extended: true}));
 
 // App checks for "ejs" files in the view so we don't have to manually type '.ejs'
 app.set("view engine", "ejs");
 
 // Serve everything in the public directory
 app.use(express.static(__dirname + '/public'));
+
+
+// Set up Session and Passport Authentication with Users
+var session = require("express-session");
+
+app.use(session({
+    secret: "Once again another passowrd",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// MIDDLEWARE that will run for every single route
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+
+    // Middleware is ran and then next route
+    next();
+});
+
+
+
+
 
 // Landing Page
 app.get('/', function(req, res) {
@@ -33,13 +77,37 @@ app.get("/create", function(req, res) {
     res.sendFile('workspace.html', {root : __dirname + '/public/html'});
 });
 
+
+app.get("/learn", function(req, res) {
+    res.render("learn");
+});
+
+
 // Login Page
 app.get("/login", function(req, res) {
     res.render("login");
 });
 
-app.post("/login", function(req, res) {
-    res.send("LOGGING IN ATTEMPT");
+// Profile Page
+app.get("/profile", function(req, res) {
+    if (res.locals.currentUser) {
+        res.render("profile");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login"
+}), function(req, res) {}
+);
+
+// Logout Page
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/home");
 });
 
 // Register Page
@@ -48,8 +116,33 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    res.send("REGISTERING ATTEMPT   ");
+    var newUser = new User({username: req.body.username});
+
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.redirect("register");
+        }
+        
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/home");
+        })
+    })
 });
+
+
+// Images Page
+app.get("/images", function(req, res) {
+    res.send("Images Page");
+});
+
+app.get("/images/:id", function(req, res) {
+    res.send("Image : " + req.params.id);
+});
+
+
+
+
 
 // Save Page
 app.get("/save", function(req, res) {
--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String
 });
 
-//module.exports = mongoose.model("User", UserSchema);
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
